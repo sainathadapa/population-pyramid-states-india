2d890b0bb977a7a6f74ab400286ee129479a97ad
Sainath Adapa <adapasainath@gmail.com>
2016-11-19 14:00:38 +0530

    obtained the list of datasets required

---
+++ 001_get_dataset_links.R
@@ -0,0 +1,30 @@
+library(ogdindiar)
+library(magrittr)
+library(dplyr)
+library(stringr)
+
+all_datasets <-  get_datasets_from_a_catalog(
+  catalog_link = 'https://data.gov.in/catalog/population-single-year-age-residence-and-sex-india-and-states',
+  limit_dataset_pages = Inf,
+  limit_datasets = Inf)
+
+all_datasets <- all_datasets %>% distinct %>% arrange(name)
+
+all_datasets <- all_datasets %>% 
+  mutate(year = str_extract(name, '[0-9]+')) %>% 
+  mutate(state = str_replace_all(name, '.*[0-9]+ - (.*)', '\\1')) %>% 
+  mutate(name = str_replace_all(name, '(.*)[0-9]{4}.*', '\\1') %>% str_trim) %>%
+  select(name, state, year, excel)
+
+req_datasets <- all_datasets %>% 
+  filter(name %in% "Population in single year age by Residence and Sex,") %>% 
+  select(-name) %>% 
+  arrange(state, year)
+
+req_datasets <- req_datasets %>% 
+  group_by(state) %>% 
+  filter(year == max(year)) %>% 
+  ungroup
+
+
+write.csv(req_datasets, file = 'dataset_list.csv', row.names = FALSE)
