ab50d236ee75295eb358a38182f4b4ca9449d2bb
Sainath Adapa <adapasainath@gmail.com>
2016-12-31 12:07:49 +0530

    changed break points and updates plots

--- 004_plot_by_mean_age.R
+++ 004_plot_by_mean_age.R
@@ -14,11 +14,11 @@ base_data <- base_data %>% filter(!(state %in% 'INDIA'))
 
 base_data %>% 
   filter(!(age %in% c('Age not stated', 'All ages'))) %>% 
-  mutate(age = ifelse(age %in% '100+', 100, age)) %>% 
+  mutate(age = ifelse(age %in% '100+', 101, age)) %>% 
   mutate(age = age %>% as.numeric) %>% 
   mutate(age = cut(age,
-                   breaks = seq(from = 0, to = 100, by = 5),
-                   include.lowest = TRUE)) %>% 
+                   breaks = seq(from = 0, to = 105, by = 5),
+                   include.lowest = TRUE, right = FALSE)) %>% 
   select(state, age, total_males, total_persons) %>% 
   group_by(state, age) %>% 
   summarise(total_males = sum(total_males),
@@ -26,6 +26,21 @@ base_data %>%
   ungroup %>% 
   mutate(total_females = total_persons - total_males) -> 
   summarised_data
+
+age_levels <- summarised_data$age %>% levels
+age_labels <- summarised_data$age %>%
+  levels %>% 
+  str_replace_all(fixed('['), '') %>% 
+  str_replace_all(fixed(')'), '') %>% 
+  str_replace_all(fixed(']'), '') %>% 
+  str_split_fixed(',', 2) %>% 
+  as_data_frame %>% 
+  mutate(V1 = as.numeric(V1),
+         V2 = as.numeric(V2) - 1) %>% 
+  mutate(ans = paste0(V1, '-' , V2)) %>% 
+  extract2('ans')
+age_labels[21] <- '100+'
+summarised_data$age <- factor(as.character(summarised_data$age), age_levels, age_labels, ordered = TRUE)
 
 mean_rank <- base_data %>% 
   filter(!(age %in% c('Age not stated', 'All ages'))) %>% 
@@ -63,19 +78,51 @@ gg <- ggplot(toplot) +
   geom_bar(aes(x = age, y = total_persons), stat = 'identity') +
   facet_wrap(~state) +
   coord_cartesian(expand = FALSE) +
-  theme_bw() +
+  theme_bw(base_size = 9) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0),
         plot.title = element_text(hjust = 0.5)) +
   ylab('% of Total population') +
   xlab('Age') + 
   scale_y_continuous(labels = function(breaks) breaks %>% multiply_by(100) %>% round(digits = 0) %>% paste0('%')) +
-  scale_x_discrete(labels = NULL)
+  scale_x_discrete(breaks = levels(toplot$age)[seq(from = 1, to = 21, by = 2)],
+                   labels = levels(toplot$age)[seq(from = 1, to = 21, by = 2)]) +
+  ggtitle('States & UTs sorted by mean age')
 
-png(filename = 'temp.png', width = 1600, height = 900)
+svglite('states-sorted-mean-age.svg')
 print(gg)
 dev.off()
 
-svglite('temp2.svg')
+toplot <- summarised_data %>% 
+  mutate(state = factor(state, levels = mean_rank$state, ordered = TRUE, labels = state_names)) %>% 
+  group_by(state) %>% 
+  mutate(total_males   = total_males/sum(total_persons),
+         total_females = total_females/sum(total_persons)) %>% 
+  ungroup %>% 
+  select(-total_persons) %>% 
+  gather(key = 'var', value = 'val', -state, -age) %>% 
+  arrange(state, age)
+
+gg <- toplot %>% 
+  mutate(val = ifelse(var %in% 'total_males', -val, val)) %>% 
+  ggplot() +
+  geom_bar(aes(x = age, y = val, fill = var),  stat = 'identity', width = 1) +
+  scale_fill_manual(values = c(total_males = '#7986CB', total_females = '#FFB74D'),
+                    labels = c(total_males = 'Male',    total_females = 'Female'),
+                    name = '') +
+  coord_flip() +
+  facet_wrap(~state) +
+  scale_y_continuous(labels = function(breaks) breaks %>% multiply_by(100) %>% abs %>% round(digits = 0) %>% paste0('%')) +
+  scale_x_discrete(breaks = levels(toplot$age)[seq(from = 1, to = 21, by = 4)],
+                   labels = levels(toplot$age)[seq(from = 1, to = 21, by = 4)]) +
+  theme_bw(base_size = 9) +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.9, 0.08),
+        legend.direction = 'vertical') +
+  ylab('% Population') +
+  xlab('Age') +
+  ggtitle('States & UTs sorted by mean age')
+
+svglite('states-sorted-mean-age2.svg')
 print(gg)
 dev.off()
 
